c0c283def769b77937ce41e0916b847e3b5edfb8
camel113 <adrienbigler@gmail.com>
2017-06-14 13:27:25 +0200

    console log

--- src/Parser.jsx
+++ src/Parser.jsx
@@ -64,7 +64,7 @@ export default class Parser extends Component {
     this.parsed = true;
 
     const parseResponse = facebook.parse(container);
-    console.log(this.props)
+    console.log(parseResponse)
     console.log("#####################")
     const { onParse } = this.props;
     if (onParse) {
